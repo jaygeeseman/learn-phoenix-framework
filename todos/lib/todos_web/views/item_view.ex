64a2ce159313b032f4646eac0f1cdbcf51caf056
Jay Geeseman <jaygeeseman@users.noreply.github.com>
2022-09-27 07:27:25 +0000

    Step 4 in progress

--- todos/lib/todos_web/views/item_view.ex
+++ todos/lib/todos_web/views/item_view.ex
@@ -1,3 +1,17 @@
 defmodule TodosWeb.ItemView do
   use TodosWeb, :view
+
+  def complete(item) do
+    case item.status do
+      1 -> "completed"
+      _ -> ""
+    end
+  end
+
+  def checked(item) do
+    case item.status do
+      1 -> true
+      _ -> false
+    end
+  end
 end
